8b3d6488760a1e4cadee353b66902700d75db440
ksharath1999 <ksharath1999@gmail.com>
2026-04-07 20:24:34 +0530

    Improved State

--- graph/state.py
+++ graph/state.py
@@ -1,11 +1,17 @@
-from typing import TypedDict
+from typing import TypedDict, Optional
 
 class AgentState(TypedDict):
     bug_report: dict
     logs: str
+
     triage: dict
     log_analysis: dict
+
     repro_output: str
+    repro_success: bool
+
     fix_plan: dict
     critic: dict
+
+    decision: Optional[str]  # NEW
     final_output: dict
